f7631b02c9fbc885e912d250012ce55dfa907df1
superH-text <108380766+superH-text@users.noreply.github.com>
2023-10-09 15:28:03 +0800

    创建了一个User实体类

---
+++ app/src/main/java/com/xxxy/lihang/chap_uint/model/User.java
@@ -0,0 +1,67 @@
+package com.xxxy.lihang.chap_uint.model;
+
+public class User {
+    int id;
+    int age;
+    String username;
+    String password;
+    String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", age=" + age +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+    public User(int id, int age, String username, String password, String address) {
+        this.id = id;
+        this.age = age;
+        this.username = username;
+        this.password = password;
+        this.address = address;
+    }
+}
