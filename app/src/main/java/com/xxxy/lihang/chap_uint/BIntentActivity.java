7d1a18b922ec70dabec1ba1b3fa17b9f12de2324
superH-text <108380766+superH-text@users.noreply.github.com>
2023-10-09 15:11:20 +0800

    进行BIntent等系列文件更改,完成实现传值功能

--- app/src/main/java/com/xxxy/lihang/chap_uint/BIntentActivity.java
+++ app/src/main/java/com/xxxy/lihang/chap_uint/BIntentActivity.java
@@ -2,7 +2,10 @@ package com.xxxy.lihang.chap_uint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BIntentActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class BIntentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bintent);
+
+        //如何拿到intent的值
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        //Toast.makeText(BIntentActivity.this,message,Toast.LENGTH_LONG).show();
+
+        //把message的值绑定到  textView上即可
+
+        TextView tv_b_show = findViewById(R.id.tv_b_show);
+        tv_b_show.setText("A:      "+message);
+
+
     }
 }
